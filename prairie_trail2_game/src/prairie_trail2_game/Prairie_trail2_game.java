8b865d3c1c2bbe1f9205cd40a159e9c17835d8f1
ajorgensen5 <ajorgensen5@msn.com>
2018-05-15 20:46:33 -0600

    5-15-2018 Created Project

---
+++ prairie_trail2_game/src/prairie_trail2_game/Prairie_trail2_game.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prairie_trail2_game;
+
+/**
+ *
+ * @author aimeejorgensen
+ */
+public class Prairie_trail2_game {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
