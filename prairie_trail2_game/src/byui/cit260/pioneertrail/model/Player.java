670b31668e9fbc44b4f7f07c96b1d622c748a332
ajorgensen5 <ajorgensen5@msn.com>
2018-05-15 20:46:33 -0600

    Added the Player Class.

---
+++ prairie_trail2_game/src/byui/cit260/pioneertrail/model/Player.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.pioneertrail.model;
+
+import java.util.Objects;
+
+/**
+ *
+ * @author aimeejorgensen
+ */
+public class Player implements Serializable {
+    private String name;
+    private int health;
+
+    public Player() {
+    }
+
+    
+    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+        
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 89 * hash + Objects.hashCode(this.name);
+        hash = 89 * hash + this.health;
+        return hash;
+    }
+
+    @Override
+    public String toString() {
+        return "Player{" + "name=" + name + ", health=" + health + '}';
+    }
+    
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Player other = (Player) obj;
+        if (this.health != other.health) {
+            return false;
+        }
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+    
+}
