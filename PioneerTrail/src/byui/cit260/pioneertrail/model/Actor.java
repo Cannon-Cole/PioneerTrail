2f1ba052b3f7980af62aac3bc31ae15b95430fc6
Cole <2bigshot@192.168.0.108>
2018-05-17 22:22:53 -0600

    Actor and Player fixed

--- PioneerTrail/src/byui/cit260/pioneertrail/model/Actor.java
+++ PioneerTrail/src/byui/cit260/pioneertrail/model/Actor.java
@@ -13,63 +13,33 @@ import java.awt.Point;
  */
 public enum Actor {
     
-Father("Brigham", "healthy", false, true, 100, new Point(1,1)), 
-Mother("Sarah", "healthy", false, true, 75, new Point(1,1)), 
-Child1("Hyrum", "healthy", false, true, 100, new Point(1,1)), 
-Child2("Anna", "healthy", false, true, 75, new Point(1,1)),
-Child3("Martin", "healthy", false, true, 50, new Point(1,1)); 
-        
-
-
-
+Father("Brigham", new Point(1,1)), 
+Mother("Sarah", new Point(1,1)), 
+Child1("Hyrum", new Point(1,1)), 
+Child2("Anna", new Point(1,1)),
+Child3("Martin", new Point(1,1)); 
 
     private String name;
-    private String ailmentType;
-    private boolean afflicted;
-    private boolean alive;
-    private double maxWeight;
     private Point coordinates;
 
- Actor(String name, String ailmentType, boolean afflicted, boolean alive, double maxWeight, Point coordinates) {
+ Actor(String name, Point coordinates) {
    
     this.name = name;
-    this.ailmentType = ailmentType;
-    this.afflicted = afflicted;
-    this.alive = alive;
-    this.maxWeight = maxWeight;
     this.coordinates = coordinates;
 
 }
 
     @Override
     public String toString() {
-        return "Actor{" + "name=" + name + ", ailmentType=" + ailmentType + ", afflicted=" + afflicted + ", alive=" + alive + ", maxWeight=" + maxWeight + ", coordinates=" + coordinates + '}';
+        return "Actor{" + "name=" + name + ", coordinates=" + coordinates + '}';
     }
 
     public String getName() {
         return name;
     }
 
-    public String getAilmentType() {
-        return ailmentType;
-    }
-
-    public boolean isAfflicted() {
-        return afflicted;
-    }
-
-    public boolean isAlive() {
-        return alive;
-    }
-
-    public double getMaxWeight() {
-        return maxWeight;
-    }
-
     public Point getCoordinates() {
         return coordinates;
     }
-    
-
 
 }
