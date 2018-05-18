12d9b987b5dc5af81dbda40e641d1c6185694d1b
Cole <2bigshot@192.168.0.108>
2018-05-17 23:07:04 -0600

    Added comments

--- PioneerTrail/src/pioneertrail/PioneerTrail.java
+++ PioneerTrail/src/pioneertrail/PioneerTrail.java
@@ -26,25 +26,28 @@ public class PioneerTrail {
     
     static void teamTest(){
        
+        //create instances
+        Player playerOne = new Player();
+        Game gameOne = new Game();
         
-       Player playerOne = new Player();
-       Game gameOne = new Game();
-        
-       playerOne.setName("Jim John");
-       playerOne.setHealth(1);
-       playerOne.setAilmentType("Broken Bone");
-       playerOne.setMaxWeight(300);
-       System.out.println(playerOne.toString());
+        //Testing for Player class
+        playerOne.setName("Jim John");
+        playerOne.setHealth(1);
+        playerOne.setAilmentType("Broken Bone");
+        playerOne.setMaxWeight(300);
+        System.out.println(playerOne.toString());
+         
+        //Testing for Game class
+        gameOne.setNumberOfPioneers(5);
+        gameOne.setTimeOfYear(6);
+        System.out.println(gameOne.toString());
        
-       gameOne.setNumberOfPioneers(5);
-       gameOne.setTimeOfYear(6);
-       System.out.println(gameOne.toString());
-       
-       System.out.println(Actor.Father.toString());
-       System.out.println(Actor.Mother.toString());
-       System.out.println(Actor.Child1.toString());
-       System.out.println(Actor.Child2.toString());
-       System.out.println(Actor.Child3.toString());
+        //Testing for Actor Enum
+        System.out.println(Actor.Father.toString());
+        System.out.println(Actor.Mother.toString());
+        System.out.println(Actor.Child1.toString());
+        System.out.println(Actor.Child2.toString());
+        System.out.println(Actor.Child3.toString());
 
 }
     
