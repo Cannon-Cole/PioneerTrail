8f989d582b57d754771a342bb6ad6f2c911c4bb9
ajorgensen5 <ajorgensen5@msn.com>
2018-05-17 21:43:05 -0600

    Project work

--- PioneerTrail/src/byui/cit260/pioneertrail/model/Actor.java
+++ PioneerTrail/src/byui/cit260/pioneertrail/model/Actor.java
@@ -15,11 +15,11 @@ public enum Actor {
     
 Father("Brigham", "healthy", false, true, 100, new Point(1,1)), 
 Mother("Sarah", "healthy", false, true, 75, new Point(1,1)), 
-Brother("Hyrum", "healthy", false, true, 100, new Point(1,1)), 
-Sister("Anna", "healthy", false, true, 75, new Point(1,1)),
-Brother("Martin", "healthy", false, true, 50, new Point(1,1)); 
+Child1("Hyrum", "healthy", false, true, 100, new Point(1,1)), 
+Child2("Anna", "healthy", false, true, 75, new Point(1,1)),
+Child3("Martin", "healthy", false, true, 50, new Point(1,1)); 
         
-}
+
 
 
 
@@ -40,6 +40,35 @@ Brother("Martin", "healthy", false, true, 50, new Point(1,1));
     this.coordinates = coordinates;
 
 }
+
+    @Override
+    public String toString() {
+        return "Actor{" + "name=" + name + ", ailmentType=" + ailmentType + ", afflicted=" + afflicted + ", alive=" + alive + ", maxWeight=" + maxWeight + ", coordinates=" + coordinates + '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAilmentType() {
+        return ailmentType;
+    }
+
+    public boolean isAfflicted() {
+        return afflicted;
+    }
+
+    public boolean isAlive() {
+        return alive;
+    }
+
+    public double getMaxWeight() {
+        return maxWeight;
+    }
+
+    public Point getCoordinates() {
+        return coordinates;
+    }
     
 
 
