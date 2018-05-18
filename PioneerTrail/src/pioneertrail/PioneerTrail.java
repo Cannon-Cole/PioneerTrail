f6867a8b99741f4fa79538a7883e1b9f38bd8606
Cole <2bigshot@192.168.0.108>
2018-05-17 23:00:54 -0600

    Added testing

--- PioneerTrail/src/pioneertrail/PioneerTrail.java
+++ PioneerTrail/src/pioneertrail/PioneerTrail.java
@@ -5,6 +5,8 @@
  */
 package pioneertrail;
 
+import byui.cit260.pioneertrail.model.Actor;
+import byui.cit260.pioneertrail.model.Game;
 import byui.cit260.pioneertrail.model.Player;
 
 /**
@@ -17,17 +19,33 @@ public class PioneerTrail {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+       
+        teamTest();
+
+    }
+    
+    static void teamTest(){
+       
         
        Player playerOne = new Player();
-       
+       Game gameOne = new Game();
+        
        playerOne.setName("Jim John");
        playerOne.setHealth(1);
        playerOne.setAilmentType("Broken Bone");
        playerOne.setMaxWeight(300);
        System.out.println(playerOne.toString());
        
-        
-        // TODO code application logic here
-    }
+       gameOne.setNumberOfPioneers(5);
+       gameOne.setTimeOfYear(6);
+       System.out.println(gameOne.toString());
+       
+       System.out.println(Actor.Father.toString());
+       System.out.println(Actor.Mother.toString());
+       System.out.println(Actor.Child1.toString());
+       System.out.println(Actor.Child2.toString());
+       System.out.println(Actor.Child3.toString());
+
+}
     
 }
