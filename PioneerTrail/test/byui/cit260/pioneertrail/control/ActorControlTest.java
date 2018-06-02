c68a9f382d514b88a8f7ce4d72dd440c272e68e0
Cole <2bigshot@192.168.0.108>
2018-06-01 21:39:25 -0600

    Added test cases to illnessHeal

--- PioneerTrail/test/byui/cit260/pioneertrail/control/ActorControlTest.java
+++ PioneerTrail/test/byui/cit260/pioneertrail/control/ActorControlTest.java
@@ -22,15 +22,69 @@ public class ActorControlTest {
      */
     @Test
     public void testIllnessHeal() {
-        System.out.println("illnessHeal");
-        double foodAmount = 0.0;
-        double mortalityRate = 0.0;
-        double medicineAmount = 0.0;
-        double expResult = 0.0;
+        System.out.println("illnessHealPass");
+        double foodAmount = 100.0;
+        double mortalityRate = 75;
+        double medicineAmount = 1;
+        double expResult = 1.63;
         double result = ActorControl.illnessHeal(foodAmount, mortalityRate, medicineAmount);
-        assertEquals(expResult, result, 0.0);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        assertEquals(expResult, result, 0.1);
+        
+        System.out.println("illnessHeal");
+        foodAmount = 100.0;
+        mortalityRate = 75;
+        medicineAmount = 1;
+        expResult = 1.63;
+        result = ActorControl.illnessHeal(foodAmount, mortalityRate, medicineAmount);
+        assertEquals(expResult, result, 0.1);
+   
+        System.out.println("illnessHealInvalid - first test");
+        foodAmount = 501.0;
+        mortalityRate = 100;
+        medicineAmount = 0;
+        expResult = -1;
+        result = ActorControl.illnessHeal(foodAmount, mortalityRate, medicineAmount);
+        assertEquals(expResult, result, 0.1);
+
+        System.out.println("illnessHealInvalid - second test");
+        foodAmount = 100.0;
+        mortalityRate = 75;
+        medicineAmount = 1;
+        expResult = 1.63;
+        result = ActorControl.illnessHeal(foodAmount, mortalityRate, medicineAmount);
+        assertEquals(expResult, result, 0.1);
+        
+        System.out.println("illnessHealInvalid - third test");
+        foodAmount = 150.0;
+        mortalityRate = 50.0;
+        medicineAmount = 0;
+        expResult = -2;
+        result = ActorControl.illnessHeal(foodAmount, mortalityRate, medicineAmount);
+        assertEquals(expResult, result, 0.1);
+
+        System.out.println("illnessHealBoundary - first test");
+        foodAmount = 200;
+        mortalityRate = 75;
+        medicineAmount = 0;
+        expResult = 2.66;
+        result = ActorControl.illnessHeal(foodAmount, mortalityRate, medicineAmount);
+        assertEquals(expResult, result, 0.1);
+
+        System.out.println("illnessHealBoundary - second test");
+        foodAmount = 225;
+        mortalityRate = 125;
+        medicineAmount = 0;
+        expResult = 1.8;
+        result = ActorControl.illnessHeal(foodAmount, mortalityRate, medicineAmount);
+        assertEquals(expResult, result, 0.1);
+
+        System.out.println("illnessHealBoundary - third test");
+        foodAmount = 500;
+        mortalityRate = 100;
+        medicineAmount = 1;
+        expResult = 5.3;
+        result = ActorControl.illnessHeal(foodAmount, mortalityRate, medicineAmount);
+        assertEquals(expResult, result, 0.1); 
     }
     
 }
