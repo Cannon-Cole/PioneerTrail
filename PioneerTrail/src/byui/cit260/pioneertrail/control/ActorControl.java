26ba8929dd2b1da3b115b16befb453e1e609b166
Cole <2bigshot@192.168.0.108>
2018-06-01 21:45:59 -0600

    indents

--- PioneerTrail/src/byui/cit260/pioneertrail/control/ActorControl.java
+++ PioneerTrail/src/byui/cit260/pioneertrail/control/ActorControl.java
@@ -12,55 +12,37 @@ package byui.cit260.pioneertrail.control;
 public class ActorControl {
    
     public static double illnessHeal(double foodAmount, double mortalityRate, double medicineAmount) {
-    /*    
-        if {(food is <= 0 || > 500)
-	RETURN -1 //food invalid
 
-IF (mortality is < 75 OR > 125)
-	RETURN -2 //mortality invalid
-
-IF (medicine is < 0)
-	RETURN -3 //medicine invalid
-IF (medicine is > 0)
-	medicineChance = .3
-
-liveOrDie = foodAmount/mortalityRate + medicineChance
-
-IF liveOrDie >= 0.5
-	RETURN liveOrDie
-ELSE
-	RETURN 0 //dies  //int
-
-    */
-    double healChance = 0;
+        
+        double healChance = 0;
     
     
-    if (foodAmount <= 0 || foodAmount > 500) {
-        return -1;
-    }
-
-    if (mortalityRate < 75 || mortalityRate > 125) {
-	return -2;
-    }
-
-    if (medicineAmount < 0) {
-	return -3 ;
-    }
-
-    if (medicineAmount > 0) {
-	healChance = .3;
+        if (foodAmount <= 0 || foodAmount > 500) {
+          return -1;
         }
 
-    double liveOrDie = foodAmount/mortalityRate + healChance;
+        if (mortalityRate < 75 || mortalityRate > 125) {
+        return -2;
+        }
+
+        if (medicineAmount < 0) {
+        return -3 ;
+        }
+
+        if (medicineAmount > 0) {
+            healChance = .3;
+        }
+
+        double liveOrDie = foodAmount/mortalityRate + healChance;
 
 
-    if (liveOrDie >= 0.5) {
-	return liveOrDie;
-    }
+        if (liveOrDie >= 0.5) {
+            return liveOrDie;
+        }
 
-    else {
-	return 0;
-    }
-    
+        else {
+            return 0;
+        }
+
     }
 }
