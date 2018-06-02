7abbbb264fba437985af90bb76e5ff703627bda9
ajorgensen5 <ajorgensen5@msn.com>
2018-06-01 20:52:34 -0600

    doubles

--- PioneerTrail/src/byui/cit260/pioneertrail/control/ActorControl.java
+++ PioneerTrail/src/byui/cit260/pioneertrail/control/ActorControl.java
@@ -11,9 +11,58 @@ package byui.cit260.pioneertrail.control;
  */
 public class ActorControl {
    
-    public static illnessHeal(food, mortality, medicine) {
-        
+    public static double illnessHeal(food, mortality, medicine) {
+    /*    
+        if {(food is <= 0 || > 500)
+	RETURN -1 //food invalid
+
+IF (mortality is < 75 OR > 125)
+	RETURN -2 //mortality invalid
+
+IF (medicine is < 0)
+	RETURN -3 //medicine invalid
+IF (medicine is > 0)
+	medicineChance = .3
+
+liveOrDie = foodAmount/mortalityRate + medicineChance
+
+IF liveOrDie >= 0.5
+	RETURN liveOrDie
+ELSE
+	RETURN 0 //dies  //int
+
+    */    
     }
  
     
+}
+
+if (food is <= 0 || food > 500) { // food is negative?
+return -1;
+}
+
+if (mortality < 75 || mortality > 125) {
+	return -2;
+}
+
+if (medicine < 0) {
+	return -3 ;
+}
+
+if (medicine > 0) {
+	medicineChance = .3;
+liveOrDie = foodAmount/mortalityRate + medicineChance;
+}
+
+if (liveOrDie >= 0.5) {
+	return liveOrDie;
+}
+ 
+    double foodAmount = assign;
+    double mortalityRate = assign;
+    double medicineChance = assign;
+
+else {
+	return 0;
+}
 }
