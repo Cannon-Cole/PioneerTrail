a3b7422b084ecade5d2735a74bd2dfb58473646a
ajorgensen5 <ajorgensen5@msn.com>
2018-06-04 16:11:05 -0600

    Fixes to equations.

--- PioneerTrail/src/byui/cit260/pioneertrail/control/LocationControl.java
+++ PioneerTrail/src/byui/cit260/pioneertrail/control/LocationControl.java
@@ -5,9 +5,12 @@
  */
 package byui.cit260.pioneertrail.control;
 
+import static java.lang.Boolean.TRUE;
+import static java.lang.Boolean.FALSE;
+
 /**
  *
- * @author aimeejorgensen
+ * @author Cole, Derek, 
  */
 public class LocationControl {
 
@@ -31,5 +34,79 @@ public class LocationControl {
             return 0;
         }
     }
+    
+    public int checkAfterHuntWeight(int estimateResources, int gatherResources, int reduceFoodSupply) {
+        
+        
+        int afterHuntWeight = (estimateResources + gatherResources - reduceFoodSupply);
    
+        
+        if (estimateResources <= 0 || estimateResources > 500) {
+	return -1;
+        }
+        
+        if (gatherResources < 5 || gatherResources > 150) {
+	return -2;
+        }
+        
+        if (reduceFoodSupply < 0 || reduceFoodSupply > 30) {
+	return -3;
+        }
+        
+        else {
+        return afterHuntWeight;
+        }
+    } 
+    
+    public static boolean checkAfterHuntSuccess(int estimateResources, int gatherResources, int reduceFoodSupply){
+        
+        int afterHuntWeight = (estimateResources + gatherResources - reduceFoodSupply);
+   
+        
+        if (afterHuntWeight >= 0.5 & afterHuntWeight <= 500) {
+	return true;
+        }
+
+        if (afterHuntWeight <=0) {
+	return false;
+        }
+    
+        else {
+	return false;
+        }
+    }
 }
+
+/*public static double illnessHeal(double foodAmount, double mortalityRate, double medicineAmount) {
+
+        double healChance = 0;
+
+        if (foodAmount <= 0 || foodAmount > 500) {
+            return -1;
+        }
+
+        if (mortalityRate < 75 || mortalityRate > 125) {
+            return -2;
+        }
+
+        if (medicineAmount < 0) {
+            return -3;
+        }
+
+        if (medicineAmount > 0) {
+            healChance = .3;
+        }
+
+        double liveOrDie = foodAmount / mortalityRate + healChance;
+
+        if (liveOrDie >= 0.5) {
+            return liveOrDie;
+        }
+
+        else {
+            return 0;
+        }
+
+    }
+}
+*/
