146b9eca50674d1283cde46919e3ce8b1d9348db
ajorgensen5 <ajorgensen5@msn.com>
2018-06-07 22:42:58 -0600

    pseudo code for algorithm.

---
+++ PioneerTrail/src/byui/cit260/pioneertrail/view/StartProgramView.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.pioneertrail.view;
+
+/**
+ *
+ * @author aimeejorgensen
+ */
+public class StartProgramView {
+
+    public StartProgramView() { //constructor
+    }
+
+    //translate statement below into java
+//    displayStartProgramView() {
+//        endOfView = false
+//        DO inputs = getInputs()
+//        IF no inputs were entered OR the first input is Q RETURN
+//        ENDIF endOfView = doAction(inputs)
+//        WHILE endOfView 
+//    != true
+//}
+//    (my attempt at the algorithm -  Aimee)
+    
+//    public void displayStartProgramView(boolean) {
+//        boolean endOfView = false;
+    
+//        do {
+//            inputs = getInputs(name);
+//    while (name == null || name == 0); 
+//    System.out.println ("Invalid name");
+//           RETURN
+//        ENDIF endOfView = doAction(inputs)
+//        WHILE endOfView 
+
+//!= true
+//}
+    
+}
