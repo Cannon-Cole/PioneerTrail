b10d45c93b3628768dd159aa7419317db9728947
derekrl <118derek@gmail.com>
2018-06-10 00:23:48 -0600

    Page 25

--- PioneerTrail/src/byui/cit260/pioneertrail/view/MainMenuView.java
+++ PioneerTrail/src/byui/cit260/pioneertrail/view/MainMenuView.java
@@ -5,13 +5,83 @@
  */
 package byui.cit260.pioneertrail.view;
 
+import java.util.Scanner;
+
 /**
  *
  * @author aimeejorgensen
  */
 public class MainMenuView {
     
-   public void displayMainMenuView(){
-       System.out.println("*** displayMainMenuView called ***");
+    public void displayMainMenuView(){
+    
+    boolean endOfView = false;
+    
+        do {
+            String[] inputs = this.getInputs();
+
+            if (inputs == null) {
+                return;
+            } else if ("E".equals(inputs[0].toUpperCase())) {
+                return;
+            }
+
+            endOfView = doAction(inputs);
+
+        }
+        while (endOfView = false);
+    
    }
+
+    private String[] getInputs() {
+        
+        Scanner reader = new Scanner(System.in);
+        String[] inputs = new String[1];
+        boolean valid = false;
+        
+        while (valid == false) {
+
+            System.out.println("  N: Start new game" +
+                    "\n  L: Load existing game" +
+                    "\n  H: Get help on how to play the game" +
+                    "\n  E: Exit");
+            inputs[0] = reader.nextLine();
+
+            inputs[0] = inputs[0].trim().toUpperCase();
+
+            if (inputs[0].length() < 1) {
+                System.out.println("You must enter a non-blank value");
+                continue;
+            }
+            
+            switch(inputs[0]) {
+            case "N":
+                System.out.println("input is N - New Game");
+                break;
+            case "L":
+                System.out.println("input is L - Load Game");
+                break;
+            case "H":
+                System.out.println("input is H - Get Help");
+                break;
+            case "E":
+                System.out.println("input is E - Exit");
+                break;
+            default:
+                System.out.println("Invalid input");
+                continue;
+            }
+            
+            valid = true;
+        }
+        
+        return inputs;
+    }
+
+    private boolean doAction(String[] inputs) {
+        System.out.println("*** MainMenuView - doAction() called ***");
+        return true;
+    }
+   
+   
 }
--- PioneerTrail/src/pioneertrail/PioneerTrail.java
+++ PioneerTrail/src/pioneertrail/PioneerTrail.java
@@ -7,6 +7,7 @@ package pioneertrail;
 
 import byui.cit260.pioneertrail.view.StartProgramView;
 import byui.cit260.pioneertrail.model.GameModel;
+import byui.cit260.pioneertrail.model.PlayerModel;
 
 /**
  *
@@ -14,12 +15,31 @@ import byui.cit260.pioneertrail.model.GameModel;
  */
 public class PioneerTrail {
 
+    private static GameModel currentGame = null;
+    private static PlayerModel player = null;
+    
     public static void main(String[] args) {
 
         StartProgramView startProgramView = new StartProgramView();
         startProgramView.displayStartProgramView();
         
-        GameModel.setPlayer(player); //save the player
+        PioneerTrail.setPlayer(player); //save the player
 
+    }
+    
+    public static GameModel getCurrentGame() {
+        return currentGame;
+    }
+    
+    public static void setCurrentGame(GameModel currentGame) {
+        PioneerTrail.currentGame = currentGame;
+    }
+    
+    public static PlayerModel getPlayer() {
+        return player;
+    }
+    
+    public static void setPlayer(PlayerModel player) {
+        PioneerTrail.player = player;
     }
 }
