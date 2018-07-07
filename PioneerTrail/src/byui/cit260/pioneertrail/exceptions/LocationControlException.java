09069b33d7eadf66f09287c00cf554a930835d8d
ajorgensen5 <ajorgensen5@msn.com>
2018-07-07 13:05:20 -0600

    checkAfterHuntWeight View

---
+++ PioneerTrail/src/byui/cit260/pioneertrail/exceptions/LocationControlException.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.pioneertrail.exceptions;
+
+/**
+ *
+ * @author aimeejorgensen
+ */
+public class LocationControlException {
+
+    public LocationControlException(String you_must_maintion_25_people_in_your_party) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    
+    }  
+
