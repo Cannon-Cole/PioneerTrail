bc19f2f776cbe51472c77d46881ec2f2c50cc3ac
ajorgensen5 <ajorgensen5@msn.com>
2018-07-14 20:46:17 -0600

    Revert "Family Report"
    This reverts commit d83c366b12ce8fff5da19c6de01d826c8a48e691.

--- PioneerTrail/src/byui/cit260/pioneertrail/view/FamilyStatusView.java
+++ PioneerTrail/src/byui/cit260/pioneertrail/view/FamilyStatusView.java
@@ -4,66 +4,22 @@
  * and open the template in the editor.
  */
 package byui.cit260.pioneertrail.view;
-import byui.cit260.pioneertrail.control.ActorControl;
-import byui.cit260.pioneertrail.model.ActorModel;
-
-import byui.cit260.pioneertrail.exceptions.ActorControlException;
-import byui.cit260.pioneertrail.model.GameModel;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import pioneertrail.PioneerTrail;
 
 /**
  *
- * @author Derek, Aimee
+ * @author Derek
  */
 public class FamilyStatusView extends View {
 
-    public FamilyStatusView() {
-
-            displayMessage = getFamilyReportString();             
-            displayMessage += "r\n  P: Print Actor Report";
-            displayMessage += "\r\n Q: Quit Menu";
+    public FamilyStatusView(String message) {
+        super(message);
     }
-    public String getFamilyReportString () {
-        String familyReport = ("\r\n****Family Actors****");
-        GameModel game = PioneerTrail.getCurrentGame();
-        ActorModel[] actorFamily = game.getFamily();
-        familyReport+= "\r\n" + String.format("%12s%5s%8s%7s%22s","Name","Hlth","Strgth","Alive","Description");
-        familyReport+= "\r\n" + String.format("%12s%5s%8s%7s%22s","----","----","------","-----","-----------");
-        for(ActorModel actor:actorFamily){
-            familyReport+= "\r\n" + String.format("%12s%5d%8d%7s%22s",
-                    actor.getName(),actor.getHealth(),actor.getStrength(),
-                    actor.isAlive()?"True":"False",actor.getDescription());  
-        }  
-         
-        return familyReport;
-    }
+    
     @Override
     public boolean doAction(String[] inputs) {
-        String input = inputs[0].toUpperCase();
-        if (input.equals("P")){
-            String fileName=getInput("Enter a file name:");
-            
-            try {
-                FileWriter report = new FileWriter(fileName);
-                report.write(getFamilyReportString());
-                report.flush();
-                report.close();
-                
-                System.out.println("You Successfully Created Your Report.");
-            } catch (IOException ex) {
-//                Logger.getLogger(FamilyStatusView.class.getName()).log(Level.SEVERE, null, ex);
-            System.out.println(ex.getMessage());
-
-            }
-            
-        }
-                
         
-        return false;
+        // advance on any input. all this does wait until the player decides to continue
+        return true;
         
     }
     
