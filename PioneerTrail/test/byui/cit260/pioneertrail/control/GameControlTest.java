aa16931c2401fa97c8ab57864a9d9ca3e00b406c
Cole <2bigshot@192.168.0.104>
2018-11-02 00:22:26 -0600

    Almost done

--- PioneerTrail/test/byui/cit260/pioneertrail/control/GameControlTest.java
+++ PioneerTrail/test/byui/cit260/pioneertrail/control/GameControlTest.java
@@ -48,82 +48,117 @@ public class GameControlTest {
         GameModel game = new GameModel();
         ArrayList<InventoryModel> inventory = InventoryControl.createInventory();
         
-        game.setWagonHealth(15);
-        game.setWagonStrength(10);
-        inventory.get(InventoryWeightPerItem.Hammer.ordinal()).setQuantity(1);
-        inventory.get(InventoryWeightPerItem.Hammer.ordinal()).setDurability(12);
-        inventory.get(InventoryWeightPerItem.SpareWheels.ordinal()).setQuantity(0);
-        int action = 2;
-        int expResult = 2;
-        int result = gameControl.repairWagonApply(game, inventory, action);
-        System.out.println("repairWagon - valid (1): " + result + ", " + expResult);
-        assertEquals(expResult, result, 0);
+        GameModel shouldBeNull = null;
+        System.out.println("Null object");
+        assertNull(shouldBeNull);
         
-        game.setWagonHealth(22);
-        game.setWagonStrength(10);
-        inventory.get(InventoryWeightPerItem.Hammer.ordinal()).setQuantity(1);
-        inventory.get(InventoryWeightPerItem.Hammer.ordinal()).setDurability(10);
-        inventory.get(InventoryWeightPerItem.SpareWheels.ordinal()).setQuantity(0);
-        action = 2;
-        expResult = -1;
-        result = gameControl.repairWagonApply(game, inventory, action);
-        System.out.println("repairWagon - invalid (2): " + result + ", " + expResult);
-        assertEquals(expResult, result, 0);
-    
-        game.setWagonHealth(15);
-        game.setWagonStrength(50);
-        inventory.get(InventoryWeightPerItem.Hammer.ordinal()).setQuantity(1);
-        inventory.get(InventoryWeightPerItem.Hammer.ordinal()).setDurability(10);
-        inventory.get(InventoryWeightPerItem.SpareWheels.ordinal()).setQuantity(0);
-        action = 2;
-        expResult = -2;
-        result = gameControl.repairWagonApply(game, inventory, action);
-        System.out.println("repairWagon - invalid (3): " + result + ", " + expResult);
-        assertEquals(expResult, result, 0);
+        GameModel shouldBeNotNull = new GameModel();
+        System.out.println("Not null object");
+        assertNotNull(shouldBeNotNull);
         
-        game.setWagonHealth(15);
-        game.setWagonStrength(12);
-        inventory.get(InventoryWeightPerItem.Hammer.ordinal()).setQuantity(1);
-        inventory.get(InventoryWeightPerItem.Hammer.ordinal()).setDurability(0);
-        inventory.get(InventoryWeightPerItem.SpareWheels.ordinal()).setQuantity(0);
-        action = 2;
-        expResult = -3;
-        result = gameControl.repairWagonApply(game, inventory, action);
-        System.out.println("repairWagon - invalid (4): " + result + ", " + expResult);
-        assertEquals(expResult, result, 0);
+        int[] arrayOne = new int[100];
+        int[] arrayTwo = new int[100];
         
-        game.setWagonHealth(5);
-        game.setWagonStrength(0);
-        inventory.get(InventoryWeightPerItem.Hammer.ordinal()).setQuantity(0);
-        inventory.get(InventoryWeightPerItem.Hammer.ordinal()).setDurability(0);
-        inventory.get(InventoryWeightPerItem.SpareWheels.ordinal()).setQuantity(1);
-        action = 1;
-        expResult = 1;
-        result = gameControl.repairWagonApply(game, inventory, action);
-        System.out.println("repairWagon - boundary (5): " + result + ", " + expResult);
-        assertEquals(expResult, result, 0);
+        for (int i = 0; i < 100; i++) {
+            arrayOne[i] = i;
+            arrayTwo[i] = i;
+        }
         
-        game.setWagonHealth(10);
-        game.setWagonStrength(19);
-        inventory.get(InventoryWeightPerItem.Hammer.ordinal()).setQuantity(1);
-        inventory.get(InventoryWeightPerItem.Hammer.ordinal()).setDurability(1);
-        inventory.get(InventoryWeightPerItem.SpareWheels.ordinal()).setQuantity(0);
-        action = 2;
-        expResult = 3;
-        result = gameControl.repairWagonApply(game, inventory, action);
-        System.out.println("repairWagon - boundary (6): " + result + ", " + expResult);
-        assertEquals(expResult, result, 0);
+        System.out.println("Array Equals");
+        assertArrayEquals(arrayOne, arrayTwo);
         
-        game.setWagonHealth(0);
-        game.setWagonStrength(10);
-        inventory.get(InventoryWeightPerItem.Hammer.ordinal()).setQuantity(0);
-        inventory.get(InventoryWeightPerItem.Hammer.ordinal()).setDurability(0);
-        inventory.get(InventoryWeightPerItem.SpareWheels.ordinal()).setQuantity(0);
-        action = 4;
-        expResult = 4;
-        result = gameControl.repairWagonApply(game, inventory, action);
-        System.out.println("repairWagon - boundary (7): " + result + ", " + expResult);
-        assertEquals(expResult, result, 0);
+        GameModel first = new GameModel();
+        GameModel second = first;
+        
+        System.out.println("Same object");
+        assertSame(first, second);
+        
+        GameModel third = new GameModel();
+        GameModel fourth = new GameModel();
+        
+        System.out.println("Not the same object");
+        assertNotSame(third, fourth);
+
+        boolean isTrue = true;
+        System.out.println("Is true");
+        assertTrue(isTrue);
+        
+//        game.setWagonHealth(15);
+//        game.setWagonStrength(10);
+//        inventory.get(InventoryWeightPerItem.Hammer.ordinal()).setQuantity(1);
+//        inventory.get(InventoryWeightPerItem.Hammer.ordinal()).setDurability(12);
+//        inventory.get(InventoryWeightPerItem.SpareWheels.ordinal()).setQuantity(0);
+//        int action = 2;
+//        int expResult = 2;
+//        int result = gameControl.repairWagonApply(game, inventory, action);
+//        System.out.println("repairWagon - valid (1): " + result + ", " + expResult);
+//        assertEquals(expResult, result, 0);
+//        
+//        game.setWagonHealth(22);
+//        game.setWagonStrength(10);
+//        inventory.get(InventoryWeightPerItem.Hammer.ordinal()).setQuantity(1);
+//        inventory.get(InventoryWeightPerItem.Hammer.ordinal()).setDurability(10);
+//        inventory.get(InventoryWeightPerItem.SpareWheels.ordinal()).setQuantity(0);
+//        action = 2;
+//        expResult = -1;
+//        result = gameControl.repairWagonApply(game, inventory, action);
+//        System.out.println("repairWagon - invalid (2): " + result + ", " + expResult);
+//        assertEquals(expResult, result, 0);
+//    
+//        game.setWagonHealth(15);
+//        game.setWagonStrength(50);
+//        inventory.get(InventoryWeightPerItem.Hammer.ordinal()).setQuantity(1);
+//        inventory.get(InventoryWeightPerItem.Hammer.ordinal()).setDurability(10);
+//        inventory.get(InventoryWeightPerItem.SpareWheels.ordinal()).setQuantity(0);
+//        action = 2;
+//        expResult = -2;
+//        result = gameControl.repairWagonApply(game, inventory, action);
+//        System.out.println("repairWagon - invalid (3): " + result + ", " + expResult);
+//        assertEquals(expResult, result, 0);
+//        
+//        game.setWagonHealth(15);
+//        game.setWagonStrength(12);
+//        inventory.get(InventoryWeightPerItem.Hammer.ordinal()).setQuantity(1);
+//        inventory.get(InventoryWeightPerItem.Hammer.ordinal()).setDurability(0);
+//        inventory.get(InventoryWeightPerItem.SpareWheels.ordinal()).setQuantity(0);
+//        action = 2;
+//        expResult = -3;
+//        result = gameControl.repairWagonApply(game, inventory, action);
+//        System.out.println("repairWagon - invalid (4): " + result + ", " + expResult);
+//        assertEquals(expResult, result, 0);
+//        
+//        game.setWagonHealth(5);
+//        game.setWagonStrength(0);
+//        inventory.get(InventoryWeightPerItem.Hammer.ordinal()).setQuantity(0);
+//        inventory.get(InventoryWeightPerItem.Hammer.ordinal()).setDurability(0);
+//        inventory.get(InventoryWeightPerItem.SpareWheels.ordinal()).setQuantity(1);
+//        action = 1;
+//        expResult = 1;
+//        result = gameControl.repairWagonApply(game, inventory, action);
+//        System.out.println("repairWagon - boundary (5): " + result + ", " + expResult);
+//        assertEquals(expResult, result, 0);
+//        
+//        game.setWagonHealth(10);
+//        game.setWagonStrength(19);
+//        inventory.get(InventoryWeightPerItem.Hammer.ordinal()).setQuantity(1);
+//        inventory.get(InventoryWeightPerItem.Hammer.ordinal()).setDurability(1);
+//        inventory.get(InventoryWeightPerItem.SpareWheels.ordinal()).setQuantity(0);
+//        action = 2;
+//        expResult = 3;
+//        result = gameControl.repairWagonApply(game, inventory, action);
+//        System.out.println("repairWagon - boundary (6): " + result + ", " + expResult);
+//        assertEquals(expResult, result, 0);
+//        
+//        game.setWagonHealth(0);
+//        game.setWagonStrength(10);
+//        inventory.get(InventoryWeightPerItem.Hammer.ordinal()).setQuantity(0);
+//        inventory.get(InventoryWeightPerItem.Hammer.ordinal()).setDurability(0);
+//        inventory.get(InventoryWeightPerItem.SpareWheels.ordinal()).setQuantity(0);
+//        action = 4;
+//        expResult = 4;
+//        result = gameControl.repairWagonApply(game, inventory, action);
+//        System.out.println("repairWagon - boundary (7): " + result + ", " + expResult);
+//        assertEquals(expResult, result, 0);
+//    }
     }
-    
 }
